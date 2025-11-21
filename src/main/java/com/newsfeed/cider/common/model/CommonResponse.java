3a6e2dc54676313576fea7e6e5a65d637f942303
LEE SEOJUN <junbeom95@gmail.com>
2025-11-21 21:11:16 +0900

    feat: CommonResponse 공통 응답 객체
    1. code - 상태 코드
    2. status - 상태
    3. content - 응답 내용

---
+++ src/main/java/com/newsfeed/cider/common/model/CommonResponse.java
@@ -0,0 +1,18 @@
+package com.newsfeed.cider.common.model;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class CommonResponse<T> {
+
+    private final int code;
+    private final HttpStatus status;
+    private final T content;
+
+    public CommonResponse(HttpStatus status, T content) {
+        this.code = status.value();
+        this.status = status;
+        this.content = content;
+    }
+}

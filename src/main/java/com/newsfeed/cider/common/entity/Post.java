6410b1d31f64eaf4d9af3b3960e17125cca99a16
Ha-minsang <gkalstkd@gmail.com>
2025-11-23 23:05:41 +0900

    refactor: 1. Comment–Post 관계를 단방향으로 변경하기 위해 OneToMany 제거 2. 생성자 Post에 profile이 제일 앞으로 오도록 순서 변경

--- src/main/java/com/newsfeed/cider/common/entity/Post.java
+++ src/main/java/com/newsfeed/cider/common/entity/Post.java
@@ -29,9 +29,6 @@ public class Post extends BaseEntity {
     @Column(nullable = false)
     private String content;             //게시글 내용
 
-    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Comment> commentList = new ArrayList<>();  //댓글 리스트
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "community_id")
     private Community community;        //커뮤니티 (그룹)
@@ -40,10 +37,10 @@ public class Post extends BaseEntity {
     private LocalDateTime deletedAt;    //삭제 여부 및 시점
 
 
-    public Post(String content, String title, Profile profile, Community community) {
+    public Post(Profile profile, String content, String title, Community community) {
+        this.profile = profile;
         this.content = content;
         this.title = title;
-        this.profile = profile;
         this.community = community;
     }
 

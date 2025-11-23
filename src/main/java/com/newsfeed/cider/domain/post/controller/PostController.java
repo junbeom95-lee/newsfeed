9fe6498ad9d30467e47b7aa7b31bd9e64da037db
Ha-minsang <gkalstkd@gmail.com>
2025-11-23 23:55:12 +0900

    feat: Post 수정용 API를 컨트롤러에 추가 Post 수정용 Request/Response DTO 추가

--- src/main/java/com/newsfeed/cider/domain/post/controller/PostController.java
+++ src/main/java/com/newsfeed/cider/domain/post/controller/PostController.java
@@ -2,17 +2,16 @@ package com.newsfeed.cider.domain.post.controller;
 
 import com.newsfeed.cider.common.model.CommonResponse;
 import com.newsfeed.cider.domain.post.model.request.PostCreateRequest;
+import com.newsfeed.cider.domain.post.model.request.PostUpdateRequest;
 import com.newsfeed.cider.domain.post.model.response.PostCreateResponse;
+import com.newsfeed.cider.domain.post.model.response.PostUpdateResponse;
 import com.newsfeed.cider.domain.post.service.PostService;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,9 +21,20 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping
-    public ResponseEntity<CommonResponse<PostCreateResponse>>createPost(
-            @Valid @RequestBody PostCreateRequest request, HttpSession session){
+    public ResponseEntity<CommonResponse<PostCreateResponse>> createPost(
+            @Valid @RequestBody PostCreateRequest request, HttpSession session
+    ) {
         Long loginId = (Long) session.getAttribute("loginId");
-return ResponseEntity.status(HttpStatus.CREATED).body(new CommonResponse<>(HttpStatus.CREATED, postService.savePost(request, loginId)));
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CommonResponse<>(HttpStatus.CREATED, postService.savePost(request, loginId)));
+    }
+
+    @PutMapping
+    public ResponseEntity<CommonResponse<PostUpdateResponse>> updatePost(
+            @Valid @RequestBody PostUpdateRequest request, HttpSession session
+    ) {
+        Long loginId = (Long) session.getAttribute("loginId");
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new CommonResponse<>(HttpStatus.OK, postService.updateService(request, loginId)));
     }
 }

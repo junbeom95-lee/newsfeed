447c9da32d156c4e63019a4ad032e59913c720f6
Ha-minsang <gkalstkd@gmail.com>
2025-11-23 23:53:33 +0900

    feat: postId로 Post를 조회하는 레포지토리 쿼리 메서드 추가

--- src/main/java/com/newsfeed/cider/domain/post/repository/PostRepository.java
+++ src/main/java/com/newsfeed/cider/domain/post/repository/PostRepository.java
@@ -3,5 +3,8 @@ package com.newsfeed.cider.domain.post.repository;
 import com.newsfeed.cider.common.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    Optional<Post> findByPostId(Long postId);
 }

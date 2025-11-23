b7dddaec8023fcbaed6af93d43baacdb4a0e4341
Ha-minsang <gkalstkd@gmail.com>
2025-11-23 23:55:41 +0900

    feat: Post 수정용 메서드 서비스에 추가 로그인한 계정이 권한이 있는지 확인하는 메서드 추가

--- src/main/java/com/newsfeed/cider/domain/post/service/PostService.java
+++ src/main/java/com/newsfeed/cider/domain/post/service/PostService.java
@@ -6,12 +6,17 @@ import com.newsfeed.cider.common.entity.Profile;
 import com.newsfeed.cider.common.enums.ExceptionCode;
 import com.newsfeed.cider.common.exception.CustomException;
 import com.newsfeed.cider.domain.post.model.request.PostCreateRequest;
+import com.newsfeed.cider.domain.post.model.request.PostUpdateRequest;
 import com.newsfeed.cider.domain.post.model.response.PostCreateResponse;
+import com.newsfeed.cider.domain.post.model.response.PostUpdateResponse;
 import com.newsfeed.cider.domain.post.repository.PostRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.newsfeed.cider.common.enums.ExceptionCode.FORBIDDEN;
+import static com.newsfeed.cider.common.enums.ExceptionCode.NOT_FOUND_POST;
 
 @Service
 @RequiredArgsConstructor
@@ -42,10 +47,41 @@ public class PostService {
         return PostCreateResponse.from(savedPost);
     }
 
+    @Transactional
+    public PostUpdateResponse updateService(@Valid PostUpdateRequest request, Long loginId) {
+
+        validateLogin(loginId);
+
+        Post post = postRepository.findByPostId(loginId)
+                .orElseThrow(() -> new CustomException(NOT_FOUND_POST));
+
+        validateAuthorization(loginId, post.getProfile().getProfileId());
+
+        // 제목 수정
+        if (request.getTitle() != null) {
+            post.updatePostTitle(request.getTitle());
+        }
+
+        // 내용 수정
+        if (request.getContent() != null) {
+            post.updatePostContent(request.getContent());
+        }
+
+        return PostUpdateResponse.from(post);
+    }
+
     // login 여부 검증
     private void validateLogin(Long loginUserId) {
         if (loginUserId == null) {
             throw new CustomException(ExceptionCode.FORBIDDEN);
+        }
+    }
+
+    // login한 profile이 권한을 가지고 있는지 검증
+    public void validateAuthorization(Long loginUserId, Long userId) {
+        boolean isSameUser = userId.equals(loginUserId);
+        if (!isSameUser) {
+            throw new CustomException(FORBIDDEN);
         }
     }
 

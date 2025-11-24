4349587ada79149ca4e78cb7a7165c993966eff0
jangse0 <syjang1016@korea.ac.kr>
2025-11-24 11:58:37 +0900

    feat: createProfile 서비스 코드 추가

--- src/main/java/com/newsfeed/cider/domain/profile/repository/ProfileRepository.java
+++ src/main/java/com/newsfeed/cider/domain/profile/repository/ProfileRepository.java
@@ -1,4 +1,12 @@
 package com.newsfeed.cider.domain.profile.repository;
 
-public class ProfileRepository {
+import com.newsfeed.cider.common.entity.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProfileRepository extends JpaRepository<Profile, Long> {
+    Optional<Profile> findByUsername(String username);
+
+    boolean existsByEmail(String email);
 }

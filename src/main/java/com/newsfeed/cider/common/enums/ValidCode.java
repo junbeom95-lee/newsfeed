d23ae2427efde48b17f3881aedabd22cb8e77f08
LEE SEOJUN <junbeom95@gmail.com>
2025-11-24 10:29:06 +0900

    feat: Valid오류에 대한 Enum 생성
    1. Validation에 대한 오류 메시지를 Enum에서 한 곳에서 관리
    2. 전역 예외 처리에서 map에 넣을 내용 ValidCode의 message를 저장

---
+++ src/main/java/com/newsfeed/cider/common/enums/ValidCode.java
@@ -0,0 +1,45 @@
+package com.newsfeed.cider.common.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum ValidCode { //Valid 오류 메시지 Enum
+
+    //프로필
+
+    //게시글
+
+    //댓글
+
+    //팔로우
+
+    //그룹
+    NOT_BLANK_COMMUNITY_NAME("communityName", "NotBlank", "커뮤니티 이름을 적어주세요");
+
+    private final String field;
+    private final String code;
+    private final String message;
+
+    ValidCode(String field, String code, String message) {
+        this.field = field;
+        this.code = code;
+        this.message = message;
+    }
+
+    /**
+     * 필드이름과 어노테이션 이름으로 메시지 얻기
+     * @param field 필드 이름
+     * @param code  어노테이션 이름
+     * @return 메시지
+     */
+    public static String getMessage(String field, String code) {
+        for (ValidCode valid : values()) {
+            if (valid.field.equals(field) && valid.code.equals(code)) {
+                return valid.message;
+            }
+        }
+        return null;
+    }
+
+}
+

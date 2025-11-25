ea778648c7401ad60d7aa8363b162a3afabc0080
Ha-minsang <gkalstkd@gmail.com>
2025-11-26 08:56:40 +0900

    feat: postLike 엔티티 및 기본 구조

--- src/main/java/com/newsfeed/cider/common/entity/PostLike.java
+++ src/main/java/com/newsfeed/cider/common/entity/PostLike.java
@@ -1,22 +1,4 @@
 package com.newsfeed.cider.common.entity;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-
-@Getter
-@Entity
-@Table(name = "post_like")
-public class PostLike extends BaseEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long likeId;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private Profile profile;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "post_id", nullable = false)
-    private Post post;
+public class PostLike {
 }

046a9de81a0c4156f104e80b208b94bb5f125531
LimYoungHun3405 <140779097+LimYoungHun3405@users.noreply.github.com>
2025-11-25 17:30:02 +0900

    Delete newsfeed-main directory

--- newsfeed-main/src/main/java/com/newsfeed/cider/common/comment/CommentRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.newsfeed.cider.common.comment;
-
-import lombok.Getter;
-
-@Getter
-public class CommentRequestDto {
-    private String content;
-    private Long parentId;  // null이면 최상위 댓글, 값 있으면 대댓글
-}

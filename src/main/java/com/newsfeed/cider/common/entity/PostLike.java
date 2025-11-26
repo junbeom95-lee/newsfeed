85b896b621f5df2f1d0a73f1d8c41524aee0c711
Ha-minsang <gkalstkd@gmail.com>
2025-11-26 12:16:55 +0900

    feat: 게시글 좋아요 기능 구현
    - PostLike 엔티티 및 응답 DTO 추가
    - 게시글 좋아요/취소 API 컨트롤러에 추가
    - 게시글 좋아요/취소 비즈니스 로직 서비스에 구현

--- src/main/java/com/newsfeed/cider/common/entity/PostLike.java
+++ src/main/java/com/newsfeed/cider/common/entity/PostLike.java
@@ -1,22 +1,31 @@
 package com.newsfeed.cider.common.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @Table(name = "post_like")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostLike extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long likeId;
+    @Column(name = "postlike_id")
+    private Long postLikeId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "profile_id", nullable = false)
     private Profile profile;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id", nullable = false)
     private Post post;
+
+    public PostLike(Profile profile, Post post) {
+        this.profile = profile;
+        this.post = post;
+    }
 }

ba1e6543c42bcb5f267add3e75eab050555ba3a5
Ha-minsang <gkalstkd@gmail.com>
2025-11-26 17:36:28 +0900

    feat: 댓글 좋아요 기능 구현
    - CommentLikeController에 댓글 좋아요/취소 API 추가
    - CommentLikeService에 좋아요 처리 및 취소 로직 구현
    - CommentLikeRepository 생성 및 JPA 메서드 추가
    - 요청/응답용 DTO 생성

--- src/main/java/com/newsfeed/cider/domain/like/model/response/CommentLikeResponse.java
+++ src/main/java/com/newsfeed/cider/domain/like/model/response/CommentLikeResponse.java
@@ -1,4 +1,17 @@
 package com.newsfeed.cider.domain.like.model.response;
 
+import lombok.Getter;
+
+@Getter
 public class CommentLikeResponse {
+
+    private final Long commentId;
+    private final boolean liked;
+    private final long likesCount;
+
+    public CommentLikeResponse(Long commentId, boolean liked, long likesCount) {
+        this.commentId = commentId;
+        this.liked = liked;
+        this.likesCount = likesCount;
+    }
 }
--- src/main/java/com/newsfeed/cider/domain/like/repository/CommentLikeRepository.java
+++ src/main/java/com/newsfeed/cider/domain/like/repository/CommentLikeRepository.java
@@ -1,7 +1,14 @@
 package com.newsfeed.cider.domain.like.repository;
 
 import com.newsfeed.cider.common.entity.CommentLike;
+import com.newsfeed.cider.common.entity.PostLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+
+    boolean existsByProfile_ProfileIdAndComment_CommentId(Long profileId, Long commentId);
+
+    long countByComment_CommentId(Long commentId);
+
+    PostLike findByProfile_ProfileIdAndComment_CommentId(Long profileId, Long commentId);
 }
